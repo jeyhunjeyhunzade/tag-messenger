335d5b30a7521c66b6608890404d9721fde0fa81
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-08-13 14:56:25 +0300

    created layout and added sidebar

---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+  return (
+    <div className="h-full w-[20%] border-l border-gray-300 bg-white p-6">
+      <div id="test" className="mb-4 flex text-2xl font-semibold">
+        TagMessenger
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
