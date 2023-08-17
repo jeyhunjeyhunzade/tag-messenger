840e58561bf1904caefd9803a038a4d4e52e0df6
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-08-17 21:37:37 +0300

    fixed automatically scroll bottom on message box

--- client/src/components/MessengerBox.tsx
+++ client/src/components/MessengerBox.tsx
@@ -1,11 +1,26 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { AppContext } from "@app/pages/App";
 
 const MessengerBox = () => {
   const { messages } = useContext(AppContext);
+  const messengerBoxRef = useRef<any>(null);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const scrollToBottom = () => {
+    if (messengerBoxRef.current) {
+      const container = messengerBoxRef.current;
+      container.scrollTop = container.scrollHeight;
+    }
+  };
 
   return (
-    <div className="messenger-box customized-scrollbar flex h-[80%] w-[90%] flex-col items-center overflow-y-auto overflow-x-hidden rounded bg-[#F0ECF7] pt-3">
+    <div
+      ref={messengerBoxRef}
+      className="messenger-box customized-scrollbar flex h-[80%] w-[90%] flex-col items-center overflow-y-auto overflow-x-hidden rounded bg-[#F0ECF7] pt-3"
+    >
       {messages.map(({ id, message }: any, i: number) => (
         <div
           key={i}
