75527a33fcecc3dcd6b9af10bc1593360b5a19b8
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-08-17 23:30:33 +0300

    prepare for production

--- server/socket.js
+++ server/socket.js
@@ -1,8 +1,18 @@
+require("dotenv").config();
 const { Server } = require("socket.io");
+
+const isProduction = process.env.NODE_ENV === "production";
+const origin = isProduction
+  ? "https://tag-messenger-client.vercel.app"
+  : "http://localhost:5173";
 
 const io = new Server(null, {
   pingInterval: 10000,
   pingTimeout: 5000,
+  cors: {
+    origin,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  },
 });
 
 const initSocketServer = (server) => {
