6b53bf3031489378468cead2e459803c528d2c23
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-08-18 00:41:21 +0300

    prepare for production

--- client/src/api/apiClient.ts
+++ client/src/api/apiClient.ts
@@ -1,9 +1,9 @@
 export const serverUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:8000"
-    : "https://tag-messenger-server.vercel.app";
+    : "https://tagmessenger-server-d4a9180982db.herokuapp.com";
 
 export const socketServerUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:8000"
-    : "wss://tag-messenger-server.vercel.app";
+    : "https://tagmessenger-server-d4a9180982db.herokuapp.com";
